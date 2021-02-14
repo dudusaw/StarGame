0cffad3bcffe17c78c4a114e9fa1e35bea52db99
Rea Thye <d.nikkit2012@gmail.com>
2021-02-14 13:12:54 +0300

    logo

--- core/src/com/mygame/screen/MenuScreen.java
+++ core/src/com/mygame/screen/MenuScreen.java
@@ -8,25 +8,30 @@ import com.badlogic.gdx.math.Vector2;
 import com.mygame.base.BaseScreen;
 import com.mygame.math.Rect;
 import com.mygame.sprite.Background;
+import com.mygame.sprite.Logo;
 
 public class MenuScreen extends BaseScreen {
 
     private Texture bg;
     private Background background;
+    private Logo logo;
 
     @Override
     public void show() {
         super.show();
         bg = new Texture("textures/bg.png");
         background = new Background(bg);
+        logo = new Logo(bg);
     }
 
     @Override
     public void render(float delta) {
+        logo.update(delta);
         Gdx.gl.glClearColor(0.3f, 0.6f, 0.4f, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
         batch.begin();
-        background.draw(batch);
+        logo.draw(batch);
+        //background.draw(batch);
         batch.end();
     }
 
@@ -38,11 +43,13 @@ public class MenuScreen extends BaseScreen {
 
     @Override
     public void resize(Rect worldBounds) {
+        logo.resize(worldBounds);
         background.resize(worldBounds);
     }
 
     @Override
     public boolean touchDown(Vector2 touch, int pointer, int button) {
-        return super.touchDown(touch, pointer, button);
+        logo.touchDown(touch, pointer, button);
+        return false;
     }
 }
